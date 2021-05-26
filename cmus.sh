2d6d625dd7c9225e4f69804b1b1f3dee2eab9c82
bosel <boselbosel@gmail.com>
2021-05-26 17:09:57 +0200

    Update cmus.sh
    workaround matching artist-sort tag

--- cmus.sh
+++ cmus.sh
@@ -4,7 +4,7 @@ prepend_zero () {
         seq -f "%02g" $1 $1
 }
 
-artist=$(echo -n $(cmus-remote -C status | grep "tag artist" | cut -c 12-))
+artist=$(echo -n $(cmus-remote -C status | grep "tag artist " | cut -c 12-))
 
 if [[ $artist = *[!\ ]* ]]; then
         song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
